082a2f16ae440b573ac7edd10f7bfdbe33af6bf9
root <roger.madjos@gmail.com>
2016-04-22 11:54:45 +0800

    refactored code to follow jscs code style configuration

--- index.js
+++ index.js
@@ -4,15 +4,15 @@
  * Module dependencies.
  */
 
-var redis = require('redis').createClient;
-var uuid = require('node-uuid');
-var async = require('async');
+var redis = require( 'redis' ).createClient;
+var uuid = require( 'node-uuid' );
+var async = require( 'async' );
 
 /**
  * Module exports.
  */
-module.exports = function(opts) {
-	return new Mutex(opts);
+module.exports = function( opts ) {
+  return new Mutex( opts );
 };
 
 /**
@@ -21,145 +21,147 @@ module.exports = function(opts) {
  * @param {Object}
  */
 
-function Mutex (opts) {
-	opts = opts || {};
+function Mutex ( opts ) {
+  opts = opts || {};
 
-	this.host = opts.host || '127.0.0.1';
-	this.port = Number(opts.port || 6379);
-	this.sleepTime = opts.sleepTime || 250;
-	this.expireTime = opts.expireTime || 5000;
-	this.prefix = opts.prefix || 'mutex:';
-	this.pub = opts.pub;
-	this.sub = opts.sub;
+  this.host = opts.host || '127.0.0.1';
+  this.port = Number( opts.port || 6379 );
+  this.sleepTime = opts.sleepTime || 250;
+  this.expireTime = opts.expireTime || 5000;
+  this.prefix = opts.prefix || 'mutex:';
+  this.pub = opts.pub;
+  this.sub = opts.sub;
 
-	//init clients if needed
-	if (!this.pub) this.pub = redis(this.port, this.host);
-	if (!this.sub) this.sub = redis(this.port, this.host);
+  //Init clients if needed
+  if ( !this.pub ) {
+    this.pub = redis( this.port, this.host );
+  }
+  if ( !this.sub ) {
+    this.sub = redis( this.port, this.host );
+  }
 
-	this.sub.subscribe(this.prefix+'release');
-	this.sub.on('message', function(_, key) {
-		if(listeners[key]) {
-			for(var k in listeners[key]) {
-		        if(listeners[key][k]) {
-		        	listeners[key][k]();
-		        }
-			}
-		}
-	});
+  this.sub.subscribe( this.prefix + 'release' );
+  this.sub.on( 'message', function( _, key ) {
+    if ( listeners[ key ] ) {
+      for ( var k in listeners[ key ] ) {
+        if ( listeners[ key ][ k ] ) {
+          listeners[ key ][ k ]();
+        }
+      }
+    }
+  } );
 }
 
 var scripts = {};
 var listeners = {};
 
-//acquire lock and set expire time
+//Acquire lock and set expire time
 scripts.acquire = {
-	script: "local locked = redis.call('SETNX', KEYS[1], ARGV[1]);" +
-  	"if locked == 1 then redis.call('PEXPIRE', KEYS[1], ARGV[2]) end;" +
-  	"return locked"
+  script: 'local locked = redis.call(\'SETNX\', KEYS[1], ARGV[1]);' +
+    'if locked == 1 then redis.call(\'PEXPIRE\', KEYS[1], ARGV[2]) end;' +
+    'return locked'
 };
 
-//should not release lock that's not yours
+//Should not release lock that's not yours
 scripts.release = {
-	script: "local id = redis.call('GET', KEYS[1]);" +
-  	"if id == ARGV[1] then redis.call('DEL', KEYS[1]) end"
+  script: 'local id = redis.call(\'GET\', KEYS[1]);' +
+    'if id == ARGV[1] then redis.call(\'DEL\', KEYS[1]) end'
 };
 
-//load lua scripts into redis
-function loadScripts(client, callback) {
-	if(scripts.acquire.sha && scripts.release.sha) {
-		return callback(null, {
-			acquire: scripts.acquire.sha,
-			release: scripts.release.sha
-		});
-	}
-	client.multi()
-		.script('LOAD', scripts.acquire.script)
-		.script('LOAD', scripts.release.script)
-		.exec(function(err, results) {
-			if(err) {
-				return callback(err);
-			}
-			scripts.acquire.sha = results[0];
-			scripts.release.sha = results[1];
-			callback(null, {
-				acquire: scripts.acquire.sha,
-				release: scripts.release.sha
-			});
-		});
+//Load lua scripts into redis
+function loadScripts ( client, callback ) {
+  if ( scripts.acquire.sha && scripts.release.sha ) {
+    return callback( null, {
+      acquire: scripts.acquire.sha,
+      release: scripts.release.sha
+    } );
+  }
+  client.multi()
+  .script( 'LOAD', scripts.acquire.script )
+  .script( 'LOAD', scripts.release.script )
+		.exec( function( err, results ) {
+  if ( err ) {
+    return callback( err );
+  }
+  scripts.acquire.sha = results[ 0 ];
+  scripts.release.sha = results[ 1 ];
+  callback( null, {
+    acquire: scripts.acquire.sha,
+    release: scripts.release.sha
+  } );
+		} );
 }
 
 /**
- * lock.
+ * Lock.
  *
  * @param string, function, number
  * @api public
  */
 
-Mutex.prototype.lock = function(key, fn, expireTime) {
-	var self = this;
-	var id = uuid.v1();
-	var original_key = key;
-	key = self.prefix+key;
-	expireTime = expireTime || self.expireTime;
+Mutex.prototype.lock = function( key, fn, expireTime ) {
+  var self = this;
+  var id = uuid.v1();
+  var originalKey = key;
+  key = self.prefix + key;
+  expireTime = expireTime || self.expireTime;
 
-	async.auto({
-		loadScripts: function(callback) {
-			loadScripts(self.pub, callback);
-		},
-		acquireLock: ['loadScripts', function(callback, results) {
-			var tout;
-			var acquire = function(){};
-			if(!listeners[key]) {
-				listeners[key] = {};
-			}
-			listeners[key][id] = acquire;
-			acquire = function() {
-				clearTimeout(tout);
-				self.pub.evalsha(results.loadScripts.acquire, 1, key, id, expireTime, function(err,result) {
-					if(err) {
-						delete listeners[key][id];
-						clearTimeout(tout);
-						if(err.toString().indexOf('NOSCRIPT') > 0) {
-							delete scripts.acquire.sha;
-							delete scripts.release.sha;
-							return callback(null, false);
-						}
-						else {
-							return callback(err);
-						}
-					}
-					if(Number(result)) {
-						delete listeners[key][id];
-						clearTimeout(tout);
-						return callback(null, true);
-					}
-					tout = setTimeout(acquire, self.sleepTime);
-				});
-			};
-			acquire();
-		}]
-	}, function(err, results) {
-		if(err) {
-			return fn(err);
-		}
-		if(results.acquireLock) {
-			var done = function() {
-				done = function(){};
-				self.pub.evalsha(results.loadScripts.release, 1, key, id, function(err) {
-					if(err) {
-						return console.error(err);
-					}
-					self.pub.publish(self.prefix+'release', key, function(err) {
-						if(err) {
-							console.error(err);
-						}
-					});
-				});
-			};
-			fn(null, done);
-		}
-		else {
-			self.lock(original_key, fn, expireTime);
-		}
-	});
+  async.auto( {
+    loadScripts: function( callback ) {
+      loadScripts( self.pub, callback );
+    },
+    acquireLock: [ 'loadScripts', function( callback, results ) {
+      var tout;
+      var acquire = function() {};
+      if ( !listeners[ key ] ) {
+        listeners[ key ] = {};
+      }
+      listeners[ key ][ id ] = acquire;
+      acquire = function() {
+        clearTimeout( tout );
+        self.pub.evalsha( results.loadScripts.acquire, 1, key, id, expireTime, function( err, result ) {
+          if ( err ) {
+            delete listeners[ key ][ id ];
+            clearTimeout( tout );
+            if ( err.toString().indexOf( 'NOSCRIPT' ) > 0 ) {
+              delete scripts.acquire.sha;
+              delete scripts.release.sha;
+              return callback( null, false );
+            }            else {
+              return callback( err );
+            }
+          }
+          if ( Number( result ) ) {
+            delete listeners[ key ][ id ];
+            clearTimeout( tout );
+            return callback( null, true );
+          }
+          tout = setTimeout( acquire, self.sleepTime );
+        } );
+      };
+      acquire();
+    } ]
+  }, function( err, results ) {
+    if ( err ) {
+      return fn( err );
+    }
+    if ( results.acquireLock ) {
+      var done = function() {
+        done = function() {};
+        self.pub.evalsha( results.loadScripts.release, 1, key, id, function( err ) {
+          if ( err ) {
+            return console.error( err );
+          }
+          self.pub.publish( self.prefix + 'release', key, function( err ) {
+            if ( err ) {
+              console.error( err );
+            }
+          } );
+        } );
+      };
+      fn( null, done );
+    }    else {
+      self.lock( originalKey, fn, expireTime );
+    }
+  } );
 };
--- test/test.js
+++ test/test.js
@@ -1,169 +1,171 @@
 /* global describe, it, before */
 'use strict';
 
-var assert = require('assert');
-var Mutex = require('../');
-var redis = require('redis').createClient;
-var async = require('async');
+var assert = require( 'assert' );
+var Mutex = require( '../' );
+var redis = require( 'redis' ).createClient;
+var async = require( 'async' );
 
-describe('Mutex Tests: ', function() {
-	this.timeout(15000);
+describe( 'Mutex Tests: ', function() {
+  this.timeout( 15000 );
 
-	var client = redis();
-	var mutex = new Mutex();
+  var client = redis();
+  var mutex = new Mutex();
 
-	before(function(done) {
-		client.eval("return redis.call('DEL', unpack(redis.call('KEYS', ARGV[1] .. '*')))", 0, mutex.prefix, function() {
-			done();
-		});
-	});
+  before( function( done ) {
+    client.eval( 'return redis.call(\'DEL\', unpack(redis.call(\'KEYS\', ARGV[1] .. \'*\')))', 0, mutex.prefix, function() {
+      done();
+    } );
+  } );
 
-	it('should apply default settings', function() {
-		var mutex = new Mutex();
-		assert.equal(mutex.prefix, 'mutex:');
-		assert.equal(mutex.sleepTime, 250);
-		assert.equal(mutex.expireTime, 5000);
-		assert.equal(mutex.host, '127.0.0.1');
-		assert.equal(mutex.port, 6379);
-		assert.equal(mutex.pub.address, '127.0.0.1:6379');
-		assert.equal(mutex.sub.address, '127.0.0.1:6379');
-	});
+  it( 'should apply default settings', function() {
+    var mutex = new Mutex();
+    assert.equal( mutex.prefix, 'mutex:' );
+    assert.equal( mutex.sleepTime, 250 );
+    assert.equal( mutex.expireTime, 5000 );
+    assert.equal( mutex.host, '127.0.0.1' );
+    assert.equal( mutex.port, 6379 );
+    assert.equal( mutex.pub.address, '127.0.0.1:6379' );
+    assert.equal( mutex.sub.address, '127.0.0.1:6379' );
+  } );
 
-	it('should apply custom settings', function() {
-		var mutex = new Mutex({
-			prefix: 'custom:',
-			sleepTime: 500,
-			expireTime: 5000,
-		});
-		assert.equal(mutex.prefix, 'custom:');
-		assert.equal(mutex.sleepTime, 500);
-		assert.equal(mutex.expireTime, 5000);
-	});
+  it( 'should apply custom settings', function() {
+    var mutex = new Mutex( {
+      prefix: 'custom:',
+      sleepTime: 500,
+      expireTime: 5000
+    } );
+    assert.equal( mutex.prefix, 'custom:' );
+    assert.equal( mutex.sleepTime, 500 );
+    assert.equal( mutex.expireTime, 5000 );
+  } );
 
-	it('should set redis lock and delete after unlock() is called', function(done) {
-		mutex.lock('test', function(err, unlock) {
-			client.exists(mutex.prefix+'test', function(_, result) {
-				assert(!!Number(result));
-				unlock();
-				setTimeout(function() {
-					client.exists(mutex.prefix+'test', function(err, result) {
-						if(err) {
-							console.error(err);
-						}
-						assert(!Number(result));
-						done();
-					});
-				}, 250);
-			});
-		});
-	});
+  it( 'should set redis lock and delete after unlock() is called', function( done ) {
+    mutex.lock( 'test', function( err, unlock ) {
+      client.exists( mutex.prefix + 'test', function( _, result ) {
+        assert( !!Number( result ) );
+        unlock();
+        setTimeout( function() {
+          client.exists( mutex.prefix + 'test', function( err, result ) {
+            if ( err ) {
+              console.error( err );
+            }
+            assert( !Number( result ) );
+            done();
+          } );
+        }, 250 );
+      } );
+    } );
+  } );
 
-	it('should reload lua scripts if needed', function(done) {
-		client.script('FLUSH', function(err) {
-			if(err) return done(err);
-			mutex.lock('test', function(err, unlock) {
-				client.exists(mutex.prefix+'test', function(_, result) {
-					assert(!!Number(result));
-					unlock();
-					setTimeout(function() {
-						client.exists(mutex.prefix+'test', function(err, result) {
-							if(err) {
-								console.error(err);
-							}
-							assert(!Number(result));
-							done();
-						});
-					}, 250);
-				});
-			});
-		});
-	});
+  it( 'should reload lua scripts if needed', function( done ) {
+    client.script( 'FLUSH', function( err ) {
+      if ( err ) {
+        return done( err );
+      }
+      mutex.lock( 'test', function( err, unlock ) {
+        client.exists( mutex.prefix + 'test', function( _, result ) {
+          assert( !!Number( result ) );
+          unlock();
+          setTimeout( function() {
+            client.exists( mutex.prefix + 'test', function( err, result ) {
+              if ( err ) {
+                console.error( err );
+              }
+              assert( !Number( result ) );
+              done();
+            } );
+          }, 250 );
+        } );
+      } );
+    } );
+  } );
 
-	it('should be able to acquire lock after previous lock expires', function(done) {
-		var timestamp = Date.now();
-		var count = 0;
-		mutex.lock('test', function(err, unlock) {
-			setTimeout(function() {
-				count++;
-				unlock();
-			}, mutex.expireTime*2);
-		});
+  it( 'should be able to acquire lock after previous lock expires', function( done ) {
+    var timestamp = Date.now();
+    var count = 0;
+    mutex.lock( 'test', function( err, unlock ) {
+      setTimeout( function() {
+        count++;
+        unlock();
+      }, mutex.expireTime * 2 );
+    } );
 
-		setTimeout(function() {
-			mutex.lock('test', function(err, unlock) {
-				assert(Date.now()-timestamp > mutex.expireTime && Date.now()-timestamp < mutex.expireTime*2);
-				setTimeout(function() {
-					assert(count > 0);
-					unlock();
-					done();
-				}, mutex.expireTime);
-			});
-		}, mutex.expireTime + mutex.expireTime/2);
-	});
+    setTimeout( function() {
+      mutex.lock( 'test', function( err, unlock ) {
+        assert( Date.now() - timestamp > mutex.expireTime && Date.now() - timestamp < mutex.expireTime * 2 );
+        setTimeout( function() {
+          assert( count > 0 );
+          unlock();
+          done();
+        }, mutex.expireTime );
+      } );
+    }, mutex.expireTime + mutex.expireTime / 2 );
+  } );
 
-	it('shoud not block executions from different scopes', function(done) {
-		var count = 0;
-		var timestamp = Date.now();
-		mutex.lock('test:0', function(err, unlock) {
-			setTimeout(function() {
-				count++;
-				unlock();
-			}, 2000);
-		});
+  it( 'shoud not block executions from different scopes', function( done ) {
+    var count = 0;
+    var timestamp = Date.now();
+    mutex.lock( 'test:0', function( err, unlock ) {
+      setTimeout( function() {
+        count++;
+        unlock();
+      }, 2000 );
+    } );
 
-		setTimeout(function() {
-			mutex.lock('test:1', function(err, unlock) {
-				assert(Date.now()-timestamp >= 800 && Date.now()-timestamp <= 1200);
-				setTimeout(function() {
-					assert(count > 0);
-					unlock();
-					done();
-				}, 2000);
-			});
-		}, 1000);
-	});
+    setTimeout( function() {
+      mutex.lock( 'test:1', function( err, unlock ) {
+        assert( Date.now() - timestamp >= 800 && Date.now() - timestamp <= 1200 );
+        setTimeout( function() {
+          assert( count > 0 );
+          unlock();
+          done();
+        }, 2000 );
+      } );
+    }, 1000 );
+  } );
 
-	it('should correctly synchronize the execution of asynchronous calls from different scopes', function(done) {
-		this.timeout(20000);
-		var count = [0,0,0];
-		async.times(30, function(index, callback) {
-			var scope = index % 3;
-			setTimeout(function() {
-				mutex.lock('test:'+scope, function(_, unlock) {
-					var num = count[scope] + 1;
-					setTimeout(function() {
-						count[scope]++;
-						assert.equal(num, count[scope]);
-						unlock();
-						callback();
-					}, 100 + Math.random()*250);
-				});
-			}, Math.random()*5000);
-		}, function() {
-			done();
-		});
-	});
+  it( 'should correctly synchronize the execution of asynchronous calls from different scopes', function( done ) {
+    this.timeout( 20000 );
+    var count = [ 0, 0, 0 ];
+    async.times( 30, function( index, callback ) {
+      var scope = index % 3;
+      setTimeout( function() {
+        mutex.lock( 'test:' + scope, function( _, unlock ) {
+          var num = count[ scope ] + 1;
+          setTimeout( function() {
+            count[ scope ]++;
+            assert.equal( num, count[ scope ] );
+            unlock();
+            callback();
+          }, 100 + Math.random() * 250 );
+        } );
+      }, Math.random() * 5000 );
+    }, function() {
+      done();
+    } );
+  } );
 
-	it('should acquire lock after previous lock expires', function(done) {
-		this.timeout(3000);
+  it( 'should acquire lock after previous lock expires', function( done ) {
+    this.timeout( 3000 );
 
-		mutex.lock('test:2', function(err, unlock) {
-			if(err) {
-				return done(err);
-			}
-			var timestamp = Date.now();
-			mutex.lock('test:2', function(err, unlock2) {
-		    if(err) {
-	        unlock();
-					return done(err);
-		    }
-				var diff = Date.now() - timestamp;
-				assert.ok(diff >= 2000, 'acquisition time should be greater than `expireTime`');
-				assert.ok(diff < 2100);
-				unlock2();
-				done();
-			});
-		}, 2000);
-	});
+    mutex.lock( 'test:2', function( err, unlock ) {
+      if ( err ) {
+        return done( err );
+      }
+      var timestamp = Date.now();
+      mutex.lock( 'test:2', function( err, unlock2 ) {
+        if ( err ) {
+          unlock();
+          return done( err );
+        }
+        var diff = Date.now() - timestamp;
+        assert.ok( diff >= 2000, 'acquisition time should be greater than `expireTime`' );
+        assert.ok( diff < 2100 );
+        unlock2();
+        done();
+      } );
+    }, 2000 );
+  } );
 
-});
+} );
